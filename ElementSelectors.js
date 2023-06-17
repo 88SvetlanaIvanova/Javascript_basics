e40b8edcd2af0a998cc9d2118968b6a6d057923d
88SvetlanaIvanova <96539287+88SvetlanaIvanova@users.noreply.github.com>
2023-06-17 10:11:42 +0300

    Create ElementSelectors.js

---
+++ ElementSelectors.js
@@ -0,0 +1,12 @@
+// ELEMENT SELECTORS
+
+// Single Element Selectors
+console.log(document.getElementById('my-form'));
+console.log(document.querySelector('.container'));
+// Multiple Element Selectors
+console.log(document.querySelectorAll('.item'));
+console.log(document.getElementsByTagName('li'));
+console.log(document.getElementsByClassName('item'));
+
+const items = document.querySelectorAll('.item');
+items.forEach((item) => console.log(item));
