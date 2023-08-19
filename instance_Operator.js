9d512fe787306a6e862ce1f524334030b9a4bf27
88SvetlanaIvanova <96539287+88SvetlanaIvanova@users.noreply.github.com>
2023-08-19 13:02:22 +0300

    Create instance_Operator.js
    instanceof

---
+++ instance_Operator.js
@@ -0,0 +1,47 @@
+
+  function User() {
+    this.active = false;
+  }
+
+  User.prototype.sayHello = function () {
+    return this.name +" says hi!";
+  }
+
+  function Student(name, major){
+    this.name = name;
+    this.major=major;
+  }
+  Student.prototype = new User();
+
+  function Teacher(name, teaching){
+    this.name = name;
+    this.teaching = teaching;
+  }
+
+  Teacher.prototype = new User();
+  Teacher.prototype.sayHello = function(){
+    return "Teacher says hi!";
+  }
+
+  let student = new Student("Peasant student", "English");
+  let teacher = new Teacher( "Calen Curry", ["math", "science"]);
+
+  console.log(student, teacher);
+  console.log(teacher instanceof Teacher);
+  console.log(teacher instanceof User);
+
+  if(teacher instanceof Student){
+    console.log("yep");
+  }else {
+    console.log("nope");
+  }
+  
+  function doSomething(user){
+    if(user instanceof User){
+      return 5;
+    }
+    return -1;
+  }
+  console.log(doSomething(teacher));
+
+  
