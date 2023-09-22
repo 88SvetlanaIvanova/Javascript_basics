d7bb0c44f3c0874be37c76b4a5c99ee11f18e2e2
88SvetlanaIvanova <96539287+88SvetlanaIvanova@users.noreply.github.com>
2023-09-22 19:06:48 +0300

    Create constructorFunction.js
    Example

---
+++ constructorFunction.js
@@ -0,0 +1,6 @@
+function HouseKeeper(name, age, yearsOfExperience, cleaningRepertoire){
+  this.name = name;
+  this.age = age;
+  this.yearsOfExperience = yearsOfExperience;
+  this.cleaningRepertoire = cleaningRepertoire;
+}
